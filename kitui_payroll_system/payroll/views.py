20d8b15292af54b5af2334ceef90631d98a5a62f
ahmednule <nuleahmed6@gmail.com>
2024-10-22 10:26:39 +0300

    added the views.py file where the home page will be the dashboard view

--- kitui_payroll_system/payroll/views.py
+++ kitui_payroll_system/payroll/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Employee, Payroll
+from .forms import EmployeeForm, PayrollForm
 
-# Create your views here.
+def dashboard(request):
+    total_employees = Employee.objects.count()
+    total_payrolls = Payroll.objects.count()
+    return render(request, 'dashboard.html', {'total_employees': total_employees, 'total_payrolls': total_payrolls})
+
+def employee_list(request):
+    employees = Employee.objects.all()
+    return render(request, 'employee_list.html', {'employees': employees})
+
+def add_employee(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('employee_list')
+    else:
+        form = EmployeeForm()
+    return render(request, 'add_employee.html', {'form': form})
+
+def payroll_view(request):
+    if request.method == 'POST':
+        form = PayrollForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+    else:
+        form = PayrollForm()
+    return render(request, 'payroll_view.html', {'form': form})
