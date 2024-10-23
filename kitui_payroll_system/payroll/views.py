61269ebb844a0d696d9719df08b09f9ba33260a2
ahmednule <nuleahmed6@gmail.com>
2024-10-23 04:48:40 +0300

    updated thetemplates to use github django_crispy_forms tags for that i could style it betterwith less code

--- kitui_payroll_system/payroll/views.py
+++ kitui_payroll_system/payroll/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa  # Ensure xhtml2pdf is installed
 from .models import Employee, Payroll
 from .forms import EmployeeForm, PayrollForm
 
@@ -25,8 +28,25 @@ def payroll_view(request):
     if request.method == 'POST':
         form = PayrollForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('dashboard')
+            payroll = form.save()
+            return generate_payroll_pdf(payroll)
     else:
         form = PayrollForm()
     return render(request, 'payroll_view.html', {'form': form})
+
+def generate_payroll_pdf(payroll):
+    template = get_template('payroll_report.html')
+    html_content = template.render({'payroll': payroll})
+
+    # Create a response object to send the PDF
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="Payroll_{payroll.employee.name}.pdf"'
+
+    # Use xhtml2pdf to generate PDF
+    pisa_status = pisa.CreatePDF(html_content, dest=response)
+
+    # Check for errors in PDF generation
+    if pisa_status.err:
+        return HttpResponse("Error generating PDF", status=500)
+
+    return response
